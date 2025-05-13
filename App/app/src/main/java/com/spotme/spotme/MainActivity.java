34ea774fa44c8d17466a8e57be678f802b898d9a
jerryurena <jerryurenaa@gmail.com>
2025-05-13 13:24:00 -0400

    Done initializing the android app layout in visual studio

---
+++ App/app/src/main/java/com/spotme/spotme/MainActivity.java
@@ -0,0 +1,13 @@
+package com.spotme.spotme;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle instance)
+    {
+        super.onCreate(instance);
+        setContentView(R.layout.main);
+    }
+}
