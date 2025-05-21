df536aad730f901bbdf6efaa0021b1b2d4bf7f28
Buchanan-M <cfscamaroimsaz@gmail.com>
2025-05-21 00:51:27 -0400

    # Basic Login Working   - Need to remove old login screens   - Need to remove old create account screen   - Need to Figure out how to use the rest of the user data to create a profile

--- App/app/src/main/java/com/spotme/spotme/createaccount.java
+++ App/app/src/main/java/com/spotme/spotme/createaccount.java
@@ -1,37 +1,82 @@
 package com.spotme.spotme;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class createaccount extends AppCompatActivity {
     TextInputEditText userEmail, userPassword;
-    Button signUpBtn = findViewById(R.id.signup);
-
-    @SuppressLint("WrongViewCast")
+    Button signUpBtn;
+    Button backToLoginButton;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_createaccount);
+        mAuth= FirebaseAuth.getInstance();
         userEmail = findViewById(R.id.new_email);
         userPassword = findViewById(R.id.create_password);
-
+        signUpBtn = findViewById(R.id.signup);
+        backToLoginButton = findViewById(R.id.return_to_login);
+        backToLoginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+               Intent  loginIntent = new Intent(getApplicationContext(), login.class);
+               startActivity(loginIntent);
+               finish();
+            }
+        });
         signUpBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 String email, password;
                 email = String.valueOf(userEmail.getText());
                 password = String.valueOf(userPassword.getText());
+
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(createaccount.this, "Enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    Toast.makeText(createaccount.this, "Enter a password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(createaccount.this, "Account Created.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(createaccount.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
             }
         });
     }
