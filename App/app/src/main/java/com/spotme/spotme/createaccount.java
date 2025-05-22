172255c5f4e4819e905ecae75d1089a0d0f716f3
Buchanan-M <cfscamaroimsaz@gmail.com>
2025-05-22 02:39:27 -0400

    # Password and Confirm Password logic implemented # Password enforcement is enabled.

--- App/app/src/main/java/com/spotme/spotme/createaccount.java
+++ App/app/src/main/java/com/spotme/spotme/createaccount.java
@@ -24,7 +24,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class createaccount extends AppCompatActivity {
-    TextInputEditText userEmail, userPassword;
+    TextInputEditText userEmail, userPassword,userConfirmPassword;
     Button signUpBtn;
     Button backToLoginButton;
     FirebaseAuth mAuth;
@@ -36,6 +36,7 @@ public class createaccount extends AppCompatActivity {
         mAuth= FirebaseAuth.getInstance();
         userEmail = findViewById(R.id.new_email);
         userPassword = findViewById(R.id.create_password);
+        userConfirmPassword = findViewById(R.id.confirm_password);
         signUpBtn = findViewById(R.id.signup);
         backToLoginButton = findViewById(R.id.return_to_login);
         backToLoginButton.setOnClickListener(new View.OnClickListener(){
@@ -49,9 +50,10 @@ public class createaccount extends AppCompatActivity {
         signUpBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                String email, password;
+                String email, password, confirmPassword;
                 email = String.valueOf(userEmail.getText());
                 password = String.valueOf(userPassword.getText());
+                confirmPassword = String.valueOf(userConfirmPassword.getText());
 
                 if (TextUtils.isEmpty(email)){
                     Toast.makeText(createaccount.this, "Enter email", Toast.LENGTH_SHORT).show();
@@ -61,22 +63,25 @@ public class createaccount extends AppCompatActivity {
                     Toast.makeText(createaccount.this, "Enter a password", Toast.LENGTH_SHORT).show();
                     return;
                 }
+                if (TextUtils.isEmpty(confirmPassword)|| !password.equals(confirmPassword)){
+                    Toast.makeText(createaccount.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 mAuth.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(createaccount.this, "Account Created.",
-                                            Toast.LENGTH_SHORT).show();
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(createaccount.this, "Account Created.",
+                                        Toast.LENGTH_SHORT).show();
 
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Toast.makeText(createaccount.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
-
-                                }
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(createaccount.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
                             }
-                        });
+                        }
+                    });
             }
         });
     }
