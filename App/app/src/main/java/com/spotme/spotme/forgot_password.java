83ba81f29e4b9d9723c11b7b8738ce83ab264ec3
Buchanan-M <cfscamaroimsaz@gmail.com>
2025-05-28 01:20:23 -0400

    # Feature Updates - Add color coding for username requirements - block creating a user account if username criteria are NG - implemented forgot user button - added basic forgot user screen - fixed formating issue with box size in username that I missed in last fix

---
+++ App/app/src/main/java/com/spotme/spotme/forgot_password.java
@@ -0,0 +1,42 @@
+package com.spotme.spotme;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.TextView;
+import android.text.Editable;
+import android.text.TextWatcher;
+
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
+
+public class forgot_password extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_forgot_password);
+
+    }
+}
