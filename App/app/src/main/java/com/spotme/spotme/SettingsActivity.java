521fe7d8add9e6a85b84c284a480200ed32a12aa
jerryurena <jerryurenaa@gmail.com>
2025-05-30 16:54:57 -0400

    deleted unused fragment class

--- App/app/src/main/java/com/spotme/spotme/SettingsActivity.java
+++ App/app/src/main/java/com/spotme/spotme/SettingsActivity.java
@@ -14,18 +14,6 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_settings);
 
-        // Example toggle listeners
-//        Switch notificationSwitch = findViewById(R.id.switchToggle);
-//        notificationSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
-//            Toast.makeText(this, "Notifications " + (isChecked ? "On" : "Off"), Toast.LENGTH_SHORT).show();
-//        });
-
-        // Example text item click
-        //LinearLayout logoutRow = findViewById(R.id.row_logout); // If you assign it an ID
-       // logoutRow.setOnClickListener(v -> {
-          //  Toast.makeText(this, "Logout clicked", Toast.LENGTH_SHORT).show();
-        //});
     }
 }
